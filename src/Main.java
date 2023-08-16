0e3b0c3d26b562bd33ae5be3587c1c2c8072fe47
Anna Korochentseva <omonrahx@gmail.com>
2023-08-16 15:09:40 +0300

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,9 @@
+public class Main {
+    public static void main(String[] args) {
+
+        int price = 7639;
+        int bonusRub = 20;
+        int totalBonus = price / bonusRub;
+        System.out.println("Начислено: " + totalBonus + " бонус(ов/a)");
+    }
+}
