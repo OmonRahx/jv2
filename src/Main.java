17f41244c364b44198b005f7e388dd7c503dc2cb
Anna Korochentseva <omonrahx@gmail.com>
2023-08-20 11:17:38 +0300

    Initial commit

--- src/Main.java
+++ src/Main.java
@@ -1,9 +1,8 @@
 public class Main {
     public static void main(String[] args) {
-
-        int price = 7639;
-        int bonusRub = 20;
-        int totalBonus = price / bonusRub;
-        System.out.println("Начислено: " + totalBonus + " бонус(ов/a)");
+        BonusMilesService service = new BonusMilesService();
+        int price = 10_000;
+        int miles = service.calculate(price); // должно получиться 500
+        System.out.println(miles);
     }
 }
